a710530ce3f6eb3dbb1a7547c0ee3aeb42800a1c
ladimiroBC <ladimiro1983@hotmail.com>
2024-01-22 16:53:38 -0500

    add toast and alert in component movies

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../domain/movie';
 import { MovieService } from '../application/movie.service';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-movies',
@@ -10,7 +11,11 @@ import { MovieService } from '../application/movie.service';
 export class MoviesPage implements OnInit {
   movies!: Movie[];
 
-  constructor(private _movieSvc: MovieService) { }
+  constructor(
+    private _movieSvc: MovieService,
+    public _toastController: ToastController,
+    public _alertController: AlertController
+    ) { }
 
   ngOnInit() {
     this.getMovies();
@@ -19,5 +24,38 @@ export class MoviesPage implements OnInit {
   getMovies(): Movie[] {
     this.movies = this._movieSvc.getMovies();
     return this.movies;
+  }
+
+  async movieToast() {
+    const toast = await this._toastController.create({
+      message: "Card movie selected",
+      duration: 2000,
+      position: "bottom"
+    })
+    toast.present();
+  }
+
+  async cardMovieDelete() {
+    const alert = await this._alertController.create({
+      header: "Card movie delete",
+      message: "You're sure",
+      buttons: [
+        {
+          text: "No",
+          handler: () => {
+            console.log("Canceled action");
+          }
+        },
+        {
+          text: "Si",
+          handler: () => {
+            console.log("Card movie delete");
+          }
+        }
+      ]
+    });
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
   }
 }
